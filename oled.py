03164c95d3571ded525f80db6a1bd42bf727367d
paulafabnunez <pnunez@gbmpaulovi.edu.ec>
2025-12-09 15:53:07 -0500

    Create oled.py

---
+++ oled.py
@@ -0,0 +1,16 @@
+from machine import Pin, I2C
+from ssd1306 import SSD1306_I2C
+import time
+# Pines I2C del ESP32 (por defecto)
+i2c = I2C(0, scl=Pin(22), sda=Pin(21), freq=400000)
+# Mostrar dispositivos I2C detectados
+print("Dispositivos I2C encontrados:", i2c.scan())
+# Inicializar display OLED 128x64
+ancho = 128
+alto = 64
+oled = SSD1306_I2C(ancho, alto, i2c)
+# Mostrar un texto simple
+oled.fill(0)  # Limpiar
+oled.text("Mocosos feos", 0, 0)
+oled.text(":3", 0, 16)
+oled.show()
