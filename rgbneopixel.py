3b7faa156987f51e8ac70a060a02a7269d32cb71
paulafabnunez <pnunez@gbmpaulovi.edu.ec>
2025-12-09 15:53:29 -0500

    Create rgbneopixel.py

---
+++ rgbneopixel.py
@@ -0,0 +1,11 @@
+import machine
+import time
+import neopixel
+
+PIN = 4
+NUM_PIX = 3
+np = neopixel.NeoPixel(machine.Pin(PIN), NUM_PIX)
+np[0] = (100, 50, 20)
+np[1] = (0, 255, 0)
+np[2] = (0, 0, 255)
+np.write()
